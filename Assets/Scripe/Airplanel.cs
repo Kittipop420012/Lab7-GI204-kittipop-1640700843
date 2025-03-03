b3bd22f3a281ee67ef3255cf7a3c8575c61ecfcd
Kittipop420012 <kittipop.thon@bumail.net>
2025-03-03 11:00:49 +0700

    airplane

--- Assets/Scripe/Airplanel.cs
+++ Assets/Scripe/Airplanel.cs
@@ -8,20 +8,36 @@ public class Airplanel : MonoBehaviour
     [SerializeField] float liftBooster = 0.5f;
     [SerializeField] float drag = 0.001f;
     [SerializeField] float airplarDrag = 0.001f;
+    [SerializeField] float angularDrag = 0.001f;
+    [SerializeField] float yawPower = 50f;
+    [SerializeField] float pitchPower = 50f;
+    [SerializeField] float rollPower = 30f;
 
     void Start()
     {
+
         rb = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
+        float yaw = Input.GetAxis("Horaizontal") * yawPower;
+        float pitch = Input.GetAxis("Vertical") * pitchPower;
+        float roll = Input.GetAxis("Roll") * rollPower;
+
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddForce(transform.forward * enginePower);
 
             Vector3 lift = Vector3.Project(rb.linearVelocity, transform.forward);
             rb.AddForce(transform.up * lift.magnitude * liftBooster);
+
+            rb.linearDamping = rb.linearVelocity.magnitude * drag;
+            rb.angularDamping = rb.linearVelocity.magnitude * angularDrag;
+
+            rb.AddTorque(transform.up * yaw);
+            rb.AddTorque(transform.right * pitch);
+            rb.AddTorque(transform.forward * roll);
         }
     }
 }
